9099f0ed1a531f3d7786e38471373dd7849fa588
айкеновад <@205_8>
2019-06-11 11:46:12 +0600

    -

--- 11.06.2019_Dictionary/Source.cpp
+++ 11.06.2019_Dictionary/Source.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<string>
 #include<fstream>
+#include<Windows.h>
 
 using namespace std;
 
@@ -12,7 +13,11 @@ using namespace std;
 //-чтение и запись словаря в файл.
 
 int main() {
-	map<string, string> dic;
+	setlocale(LC_ALL, "");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	map<string, string> dict;
 	ifstream in("Dictionary.txt");
 
 	string key, value;
@@ -20,9 +25,59 @@ int main() {
 		getline(in, key, '#');
 		getline(in, value, '#');
 		in.ignore();
-		cout << key << " " << value << endl;
+		dict[key] = value;
 	}
 
+	cout << "Enter 1 to find a word" << endl;
+	cout << "Enter 2 to remove a word" << endl;
+	cout << "Enter 3 to add a word" << endl;
+	cout << "Enter 4 to edit a word" << endl;
+	int choice;
+	cin >> choice;
+	string word;
+
+	switch (choice) {
+	case 1:
+	{
+		cin >> word;
+		auto it = dict.find(word);
+		if (it != dict.end())
+			cout << it->first << " " << it->second << endl;
+	}
+		break;
+	case 2:
+	{
+		cin >> word;
+		auto it = dict.find(word);
+		if (it != dict.end())
+			dict.erase(it);
+	}
+		break;
+	case 3:
+	{
+		cin >> key;
+		getline(cin, value, '#');
+		dict[key] = value;
+	}
+		break;
+	case 4:
+	{
+		cin >> word;
+		auto it = dict.find(word);
+
+		if (it != dict.end())
+		{
+			cout << it->second << endl;
+			getline(cin, value, '#');
+			dict[word] = value;
+		}
+		break;
+	}
+	}
+
+	cout << "DICTIONARY" << endl;
+	for (auto it = dict.begin(); it != dict.end(); it++)
+		cout << it->first << " " << it->second << endl;
 
 
 
