2d360a06fbc92a43e3618d5fbd9c2c5a3e31389f
айкеновад <@205_8>
2019-06-11 11:14:10 +0600

    -

---
+++ 11.06.2019_Dictionary/Source.cpp
@@ -0,0 +1,31 @@
+﻿#include<map>
+#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+//1.	Используя контейнер map реализовать программу – словарь.
+//Основные возможности :
+//-поиск слова;
+//-добавление, удаление, редактирование слов;
+//-чтение и запись словаря в файл.
+
+int main() {
+	map<string, string> dic;
+	ifstream in("Dictionary.txt");
+
+	string key, value;
+	while (!in.eof()) {
+		getline(in, key, '#');
+		getline(in, value, '#');
+		in.ignore();
+		cout << key << " " << value << endl;
+	}
+
+
+
+
+	system("pause");
+	return 0;
+}
